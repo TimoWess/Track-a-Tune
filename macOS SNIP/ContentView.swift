098b576d9bcd6688ea66d971b5871013b9fd182c
Timo Wesselmann <timo.wesselmann@gmail.com>
2022-03-05 12:44:38 +0100

    Initial Commit

---
+++ macOS SNIP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  macOS SNIP
+//
+//  Created by Timo Wesselmann on 05.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macOS SNIP/macOS_SNIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_SNIPApp.swift
+//  macOS SNIP
+//
+//  Created by Timo Wesselmann on 05.03.22.
+//
+
+import SwiftUI
+
+@main
+struct macOS_SNIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
